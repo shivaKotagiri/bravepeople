fba8fc894ef2a8084451e92e6181c831a8662fc6
shivaKotagiri <shivakotagiri532@gmail.com>
2025-10-25 11:28:46 +0530

    added the gsap animation for the navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,15 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Image from "next/image";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger)
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText)
 export default function Navbar() {
     const navRef = useRef<HTMLDivElement | null>(null);
+    const [hover, setHover] = useState<boolean>(false);
+    const textRef = useRef<HTMLDivElement | null>(null);
+    const splitRef = useRef<HTMLDivElement | null>(null);
 
     useGSAP(() => {
         const nav = navRef.current;
@@ -24,6 +27,58 @@ export default function Navbar() {
             },
         });
     });
+    const timelineRef = useRef<gsap.core.Timeline | null>(null);
+    useGSAP(() => {
+        if(!textRef.current) return;
+        if(!splitRef.current) return;
+
+        const splits1 = new SplitText(splitRef.current, { 
+            type: "chars, words, lines", 
+            linesClass: "line", 
+            wordsClass: "word", 
+            charsClass: "char"
+        });
+        const splits2 = new SplitText(textRef.current, { 
+            type: "chars, words, lines", 
+            linesClass: "line", 
+            wordsClass: "word", 
+            charsClass: "char"
+        });
+
+        gsap.set(splits1.chars, { y: 0 });
+        gsap.set(splits2.chars, { y: 0 });
+        
+        timelineRef.current = gsap.timeline({ paused: true });
+        
+        timelineRef.current.to(splits1.chars, {
+            duration: 0.25,
+            y: -100,
+            stagger: { amount: 0.05, from: "center" },
+            ease: "power3.inOut"
+        }).from(splits2.chars, {
+            duration: 0.25,
+            y: 100,
+            stagger: { amount: 0.05, from: "center" },
+            ease: "power3.inOut"
+        }, "-=0.25");
+
+        return () => {
+            timelineRef.current?.kill();
+            splits1.revert();
+            splits2.revert();
+        }
+    }, []); 
+
+    useGSAP(() => {
+        if(!timelineRef.current) return;
+        
+        if(hover) {
+            timelineRef.current.play();
+        } else {
+            timelineRef.current.reverse();
+        }
+    }, [hover]);
+
     return (
         <nav className="fixed w-fit shadow-lg bg-white uppercase text-sm font-medium p-[0.25rem] rounded-4xl flex flex-row gap-1">
             <div ref={navRef} className="flex justify-center items-center">
@@ -33,7 +88,13 @@ export default function Navbar() {
             <div className="hover:bg-[#F0EDE6] cursor-pointer py-3.5 px-5 rounded-3xl">about</div>
             <div className="hover:bg-[#F0EDE6] cursor-pointer py-3.5 px-5 rounded-3xl">work</div>
             <div className="hover:bg-[#F0EDE6] cursor-pointer py-3.5 px-5 rounded-3xl">insights</div>
-            <div className="hover:bg-white transition-colors duration-300 hover:text-black border cursor-pointer py-3 px-[22px] rounded-4xl bg-black text-white flex justify-center items-center">work with us</div>
+            <div 
+                onMouseEnter={() => setHover(true)} 
+                onMouseLeave={() => setHover(false)}
+                className="hover:bg-white overflow-hidden relative transition-colors duration-300 hover:text-black border cursor-pointer py-3 px-[22px] rounded-4xl bg-black text-white flex justify-center items-center w-[10rem]">
+                <div ref={splitRef} className="absolute inset-0 flex justify-center items-center z-0">work&nbsp;with&nbsp;us</div>
+                <div ref={textRef} className="absolute inset-0 flex justify-center items-center z-10">work&nbsp;with&nbsp;us</div>
+            </div>
         </nav>
     )
 }
