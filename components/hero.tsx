ac6be2d904a6086e27994a9a0df1830714297274
shivaKotagiri <shivakotagiri532@gmail.com>
2025-10-27 11:39:52 +0530

    added the text-reveal animation

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,28 +1,24 @@
 "use client";
 
-import { motion, useTransform, useScroll } from "motion/react";
+import { motion } from "motion/react";
 import Image from "next/image";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
+import { SplitText } from "gsap/all";
+
+interface SplitTextInstance {
+    lines: HTMLElement[];
+    revert: () => void;
+}
+
+gsap.registerPlugin(SplitText);
 
 export default function Hero() {
     const parentRef = useRef<HTMLDivElement | null>(null);
-    // const { scrollYProgress } = useScroll({
-    //     target: parentRef,
-    //     offset: ["start start", "end end"]
-    // });
+    const textContainerRef = useRef<HTMLDivElement | null>(null);
     const imageRef1 = useRef<HTMLImageElement | null>(null);
     const imageRef2 = useRef<HTMLImageElement | null>(null);
     const imageRef3 = useRef<HTMLImageElement | null>(null);
-
-    
-    
-    // const x1 = useTransform(scrollYProgress, [0, 1], [0, -400]);
-    // const x2 = useTransform(scrollYProgress, [0, 1], [0, -600]);
-    // const x3 = useTransform(scrollYProgress, [0, 1], [0, -800]);
-    // const x4 = useTransform(scrollYProgress, [0, 1], [0, 400]);
-    // const x5 = useTransform(scrollYProgress, [0, 1], [0, 600]);
-    // const x6 = useTransform(scrollYProgress, [0, 1], [0, 800]);
 
     useEffect(() => {
         const section = parentRef.current;
@@ -31,6 +27,7 @@ export default function Hero() {
         const image3 = imageRef3.current;
 
         if(!section || !image1 || !image2 || !image3) return;
+        
         const handleMouseMove = (e: MouseEvent) => {
             const sectionRect = section.getBoundingClientRect();
 
@@ -39,7 +36,6 @@ export default function Hero() {
             ) {
                 const sectionCenterX = sectionRect.left + sectionRect.width / 2;
                 const sectionCenterY = sectionRect.top + sectionRect.height / 2;
-
 
                 const moveX = (e.clientX - sectionCenterX) / (sectionRect.width / 2);
                 const moveY = (e.clientY - sectionCenterY) / (sectionRect.height / 2);
@@ -97,25 +93,62 @@ export default function Hero() {
         }
 
         window.addEventListener("mousemove", handleMouseMove);
-        section.addEventListener("mouseleave", handleMouseMove);
+        section.addEventListener("mouseleave", handleMouseLeave);
 
         return () => {
             window.removeEventListener("mousemove", handleMouseMove)
             section.removeEventListener("mouseleave", handleMouseLeave)
         }
     }, [])
+
+    useEffect(() => {
+        if (!textContainerRef.current) return;
+
+        const split = new SplitText(textContainerRef.current, {
+            type: "lines",
+            linesClass: "split-line"
+        }) as unknown as SplitTextInstance;
+
+        const lines = split.lines;
+
+        // Wrap each line in a mask container
+        lines.forEach(line => {
+            const wrapper = document.createElement('div');
+            wrapper.style.overflow = 'hidden';
+            wrapper.style.lineHeight = '0.85';
+            line.parentNode?.insertBefore(wrapper, line);
+            wrapper.appendChild(line);
+        });
+
+        gsap.set(lines, {
+            yPercent: 100,
+            opacity: 1
+        });
+
+        gsap.to(lines, {
+            yPercent: 0,
+            duration: 0.75,
+            ease: "power4.out",
+            stagger: 0.25
+        });
+
+        return () => {
+            split.revert();
+        };
+    }, []);
     
     return (
-        <div ref={parentRef} className="h-fit relative pb-20">
+        <div ref={parentRef} className="h-fit relative">
             <div className="h-fit">
                 <div className="mt-5">
                     <Image alt="logo" width={192} height={24} src={"/logo.svg"} />
                 </div>
-                <div className="-translate-x-3 mt-30 relative tracking-tighter font-pp-mori uppercase font-normal text-[138px] w-fit h-fit overflow-visible">
-                    <div className="flex items-center gap-5 justify-start inset-0">
-                        <motion.div
-                            // style={{ x: x1 }}
-                            className="leading-25 z-2 mr-45 relative inline-block will-change-transform">
+                <div 
+                    ref={textContainerRef}
+                    className="-translate-x-3 mt-30 relative tracking-tighter font-pp-mori uppercase font-normal text-[138px] w-fit h-fit overflow-visible leading-[0.85]"
+                >
+                    <div className="flex items-center justify-start inset-0 overflow-hidden">
+                        <motion.div className="leading-30 z-2 mr-45 relative inline-block will-change-transform">
                             launch
                         </motion.div>
                         <div style={{ perspective: '1000px' }} className="w-[171px] h-[96px] absolute left-[40%]">
@@ -129,19 +162,15 @@ export default function Hero() {
                                 <span className="text-base text-white tracking-wide absolute inset-0 flex items-center justify-center">play</span>
                             </div>
                         </div>
-                        <motion.div
-                            // style={{ x: x4 }}
-                            className="leading-0 z-2 will-change-transform">
+                        <motion.div className="leading-30 z-2 will-change-transform">
                             digital
                         </motion.div>
                     </div>
-                    <div className="flex gap-5 justify-start inset-0">
-                        <motion.div 
-                            // style={{ x: x2 }}
-                            className="leading-none z-2 mr-20 will-change-transform">
+                    <div className="flex justify-start inset-0">
+                        <motion.div className="leading-30 z-2 mr-20 will-change-transform">
                             experien­ces
                         </motion.div>
-                        <div style={{ perspective: '1000px' }} className="absolute w-[220px] h-[211px] top-[15%] left-[60%]">
+                        <div style={{ perspective: '1000px' }} className="absolute w-[220px] h-[211px] left-[60%]">
                             <Image alt="logo"
                                 ref={imageRef2} 
                                 className="z-0 h-auto hover:opacity-80 transition-opacity duration-400 object-cover"
@@ -149,19 +178,15 @@ export default function Hero() {
                                 src={"https://cdn.prod.website-files.com/658031e408a50a76013e5183/68bcc2070b51d06488307d3a_683cf5a69ff407866c3051b9e5aded55_iPhone_15_Pro_Max_10-1-copy-main2-1465x980-p-1080.jpg"} 
                             />
                         </div>
-                        <motion.div 
-                            // style={{ x: x5 }}
-                            className="leading-none z-2 will-change-transform">
+                        <motion.div className="leading-30 z-2 will-change-transform">
                             that
                         </motion.div>
                     </div>
-                    <div className="flex gap-5 justify-start inset-0">
-                        <motion.div 
-                            // style={{ x: x3 }}
-                            className="leading-25 z-2 mr-20 will-change-transform">
+                    <div className="flex justify-start inset-0 overflow-hidden">
+                        <motion.div className="leading-30 z-2 mr-20 will-change-transform">
                             shape
                         </motion.div>
-                        <div style={{ perspective: '1000px' }} className="absolute w-[204px] h-[155px] top-[60%] left-[27%]">
+                        <div style={{ perspective: '1000px' }} className="absolute w-[204px] h-[155px] left-[27%]">
                             <Image 
                                 ref={imageRef3}
                                 alt="logo" 
@@ -170,9 +195,7 @@ export default function Hero() {
                                 src={"https://cdn.prod.website-files.com/658031e408a50a76013e5183/68840d2477f6c7d14380dd64_rccl-watch2-p-800.jpg"} 
                             />
                         </div>
-                        <motion.div 
-                            // style={{ x: x6 }}
-                            className="leading-25 z-2 tracking-tighter will-change-transform">
+                        <motion.div className="leading-30 z-2 tracking-tighter will-change-transform">
                             the future.
                         </motion.div>
                     </div>
---
+++ components/ui/text-reveal.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import { useRef } from "react";
+
+interface Props {
+    children: React.ReactNode,
+    onScrollAnimation?: boolean,
+    delay?: number
+}
+
+interface SplitTextInstance {
+    lines: HTMLElement[],
+    revert: () => void
+}
+gsap.registerPlugin(ScrollTrigger, SplitText);
+export default function TextReveal({ children, onScrollAnimation = false, delay = 0 }: Props) {
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const splitRef = useRef<SplitTextInstance | null>(null);
+
+    useGSAP(() => {
+        if(!containerRef.current) return;
+        if(splitRef && splitRef.current) return splitRef.current.revert();
+        const split = new SplitText(containerRef.current, {
+            linesClass: "split-line",
+            type: "lines",
+            mask: "lines",
+        }) as unknown as SplitTextInstance;
+
+        const lines = split.lines;
+
+        gsap.set(lines, {
+            yPercent: 200,
+            opacity: 0,
+            overflow: "hidden"
+        });
+
+        const animationProps = {
+            yPercent: 0,
+            duration: 1.5,
+            ease: "power4.out",
+            opacity: 1,
+            stagger: 0.1,
+            delay
+        }
+
+        if(onScrollAnimation) {
+            gsap.to(lines, {
+                ...animationProps,
+                scrollTrigger: {
+                    start: "top 85%",
+                    trigger: containerRef.current,
+                    once: true
+                }
+            })
+        } else {
+            gsap.to(lines, animationProps);
+        }
+
+        splitRef.current = split;
+
+        return () => {
+            split.revert();
+        };
+    }, { dependencies: [onScrollAnimation, delay, children]})
+    return (
+        <div ref={containerRef} className="overflow-hidden">
+            { children }
+        </div>
+    )
+}
