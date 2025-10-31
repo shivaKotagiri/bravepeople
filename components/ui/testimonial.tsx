ceff8b135a4b3bfc0ed9bf0c20ed367ccb29d466
shivaKotagiri <shivakotagiri532@gmail.com>
2025-10-31 10:57:07 +0530

    added the text reveal scroll animation

--- components/ui/testimonial.tsx
+++ components/ui/testimonial.tsx
@@ -1,12 +1,43 @@
 "use client";
 import Image from "next/image";
 import { useRef } from "react";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(SplitText);
 
 export default function Testimonial() {
     const textRef = useRef<HTMLDivElement | null>(null);
+    useGSAP(() => {
+        if(!textRef.current) return;
+        const text:HTMLDivElement = textRef.current;
+
+        const splits = new SplitText(text, {
+            type: "lines, words, chars",
+            wordsClass: "word",
+            linesClass: "line",
+            charsClass: "char"
+        });
+
+        gsap.fromTo(splits.words, { opacity: 0.4 }, {
+            opacity: 1,
+            duration: 0.3,
+            stagger: 0.1,
+            scrollTrigger: {
+                scrub: true,
+                once: true,
+                trigger: text,
+                markers: true,
+                start: "top 20%"
+            }
+        });
+
+        return () => splits.revert();
+    }, [textRef.current])
     return (
         <div className="w-full lg:my-35 mx-auto h-fit">
-            <div ref={textRef} className="mb-15 mx-auto w-full leading-none text-[#4D4D4D] text-[3.5rem] lg:text-[5rem]">
+            <div ref={textRef} className="mb-15 mx-auto w-full leading-none text-white text-[3.5rem] lg:text-[5rem]">
                 &quot;The Brave People team has done a masterful job at capturing the essence of FCF and projecting it through our site, mobile apps and branding elements. Always game for the next challenge, they continue to work closely with our product, tech and marketing groups under aggressive timelines to deliver creative which consistently exceeds expectations and delights our fans. Simply put, Brave People crushes it.&quot;
             </div>
             <div className="flex gap-3 items-center">
