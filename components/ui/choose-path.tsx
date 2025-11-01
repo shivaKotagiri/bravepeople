6f9c44babf36510239eefc2bd8eaae34db747f23
shivaKotagiri <shivakotagiri532@gmail.com>
2025-11-01 21:22:13 +0530

    added some animaitons the project-path component & create choose-path component

---
+++ components/ui/choose-path.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { motion } from "motion/react";
+
+interface Props {
+    title: string,
+    link: string,
+    text: string,
+    para: string
+}
+
+export default function ChoosePath({ title, link, text, para}: Props) {
+    const [hover, setHover] = useState<boolean>(false);
+    return (
+        <div
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            className="col-span-1 relative w-[710px] h-[780px] border cursor-pointer hover:border-transparent border-neutral-400 rounded-[40px] text-black hover:text-white">
+            <Image 
+                className={`absolute inset-0 w-[711px] h-[780px] transition-opacity duration-500 ${hover ? "opacity-100": "opacity-0"}`}
+                alt="image1" 
+                width={711} 
+                height={780} 
+                src={link} 
+            />
+            <div className="absolute inset-0 p-15 flex flex-col justify-between items-start">
+                <div className="flex flex-col">
+                    <div className="text-6xl font-[500] mt-[30%]">{ title }</div>
+                    <div className="px-4 py-1 border w-fit rounded-2xl font-[500] mt-5">{ text }</div>
+                </div>
+                <div className="flex flex-col">
+                    <div className="max-w-lg text-2xl font-[500]">
+                        { para }
+                    </div>
+                    <motion.div
+                        initial={{width: "3.75rem"}}
+                        animate={{ width: hover ? "15rem": "3.75rem"}} 
+                        transition={{
+                            width: {
+                                duration: 0.3
+                            }
+                        }}
+                        className="border border-white overflow-hidden rounded-full relative cursor-pointer mt-10 h-[3.75rem] w-[3.75rem] flex justify-between items-center text-black bg-white hover:text-white hover:bg-transparent transition-colors duration-500 px-5">
+                        {hover && <motion.div
+                            initial={{display: "hidden", opacity: 0, y: "200%"}}
+                            animate={{ display: "block", y: "0%", opacity: 1 }}
+                            transition={{
+                                y: {
+                                    delay: 0.2,
+                                    duration: 0.25
+                                }
+                            }}
+                            >
+                            Work With Us
+                        </motion.div>}
+                        <Image
+                            className="h-[38px] w-[26px]"
+                            width={26} 
+                            height={38} 
+                            alt="Right arrow" 
+                            src={"/right-arrow.svg"} 
+                        />
+                    </motion.div>
+                </div>
+            </div>
+        </div>
+    )
+}
