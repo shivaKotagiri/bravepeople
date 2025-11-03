6f7e6d3ed8778e1dd71544854bd771fce5d4692e
shivaKotagiri <shivakotagiri532@gmail.com>
2025-11-03 16:31:49 +0530

    made the trustedby component responsive

--- components/trustedby.tsx
+++ components/trustedby.tsx
@@ -1,14 +1,23 @@
 "use client";
 import { logos } from "@/utils/data/logos";
 import Image from "next/image";
+
 export default function TrustedBy() {
     return (
-        <div className="grid lg:grid-cols-4 grid-cols-3 w-full h-screen mt-[20%] md:mt-[10%] px-[3%] 2xl:px-[5%]">
-            {logos.map((logo, index) => (
-                <div key={index} className="col-span-1 h-fit w-auto relative max-w-[325px] max-h-[230px]">
-                    <Image className="absolute inset-0 h-fit w-auto object-cover" width={325} height={230} src={logo} alt={logo} />
-                </div>
-            ))}
+        <div className="w-full my-[10%] md:my-[5%] px-[3%] 2xl:px-[5%] py-20">
+            <div className="grid grid-cols-3 lg:grid-cols-4 gap-8 md:gap-12 lg:gap-16 place-items-center">
+                {logos.map((logo, index) => (
+                    <div key={index} className="relative w-full h-auto flex items-center justify-center">
+                        <Image 
+                            className="w-full h-auto object-contain max-w-[200px] sm:max-w-[250px] lg:max-w-[325px]" 
+                            width={325} 
+                            height={230} 
+                            src={logo} 
+                            alt={`Logo ${index + 1}`} 
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
